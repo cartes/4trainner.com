03ff68246ca8191da1397d8de5c065dfb30262b6
cartes <cristiancartesa@gmail.com>
2024-10-18 12:31:32 -0300

    Roles profesor y alumno

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,7 +19,19 @@ class DatabaseSeeder extends Seeder
             'guard_name' => 'web', // Añadir el guard_name explícitamente
         ]);
 
-        // Crear un usuario y asignarle el rol si no existe ya
+        // Crear el rol de Profesor si no existe
+        $profesorRole = Role::firstOrCreate([
+            'name' => 'profesor',
+            'guard_name' => 'web',
+        ]);
+
+        // Crear el rol de Alumno si no existe
+        $alumnoRole = Role::firstOrCreate([
+            'name' => 'alumno',
+            'guard_name' => 'web',
+        ]);
+
+        // Crear un usuario y asignarle el rol de Super Admin si no existe ya
         if (!User::where('email', 'cristiancartesa@gmail.com')->exists()) {
             $user = User::create([
                 'name' => 'Admin',
@@ -28,6 +40,28 @@ class DatabaseSeeder extends Seeder
             ]);
 
             $user->assignRole($superAdminRole);
+        }
+
+        // Crear un usuario de prueba como Profesor si no existe ya
+        if (!User::where('email', 'profesor@example.com')->exists()) {
+            $profesor = User::create([
+                'name' => 'Profesor Ejemplo',
+                'email' => 'profesor@example.com',
+                'password' => bcrypt('password'),
+            ]);
+
+            $profesor->assignRole($profesorRole);
+        }
+
+        // Crear un usuario de prueba como Alumno si no existe ya
+        if (!User::where('email', 'alumno@example.com')->exists()) {
+            $alumno = User::create([
+                'name' => 'Alumno Ejemplo',
+                'email' => 'alumno@example.com',
+                'password' => bcrypt('password'),
+            ]);
+
+            $alumno->assignRole($alumnoRole);
         }
     }
 }
